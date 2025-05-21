a4b50e69af91ce1d388e08d70c6ed642418ca3f6
kamo312 <kamogelomalema1@gmail.com>
2025-05-21 13:54:03 -0400

    added close modal on click event

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 'use strict';
 
-// add button event listeners
-
+// open modal on click event
 document.querySelectorAll('.show-modal').
 forEach(e => e.addEventListener("click", () => {
     document.querySelector(".modal").classList.remove('hidden');
     document.querySelector(".overlay").classList.remove('hidden');
-}))
+}));
 
-
-// remove hidden class on click function 
-
-
-// close modal function
+// close modal on click event
+document.querySelector(".close-modal").
+    addEventListener("click", () => {
+        document.querySelector(".modal").classList.add('hidden');
+        document.querySelector(".overlay").classList.add('hidden'); 
+});
