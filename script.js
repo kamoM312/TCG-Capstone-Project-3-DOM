860864f5ae2e19fd21744c991c91565792115c84
kamo312 <kamogelomalema1@gmail.com>
2025-05-21 13:44:02 -0400

    added button click eventlistener and functionality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// add button event listeners
+
+document.querySelectorAll('.show-modal').
+forEach(e => e.addEventListener("click", () => {
+    document.querySelector(".modal").classList.remove('hidden');
+    document.querySelector(".overlay").classList.remove('hidden');
+}))
+
+
+// remove hidden class on click function 
+
+
+// close modal function
